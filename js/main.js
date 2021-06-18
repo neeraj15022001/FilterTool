df64c1dc5c7d9d82faed89d2ce4573cdc552f181
neeraj15022001 <gneeraj32595@gmail.com>
2021-06-19 00:22:30 +0530

    file now adding

--- js/main.js
+++ js/main.js
@@ -16,3 +16,24 @@ $("#toggle-drop-shadow-button").click(() => {
     .toggleClass("switch-url");
   $("#drop-shadow-section").slideToggle();
 });
+$("#change-image-button").click(() => {
+    if($("#toggler").hasClass("switch-local")) {
+        const imgPath = document.querySelector('input[type=file]').files[0];
+        const reader = new FileReader();
+        if(imgPath !== "") {
+            reader.addEventListener("load", function () {
+                // convert image file to base64 string and save to localStorage
+                localStorage.setItem("image", reader.result);
+                $("#display-image").attr("src", localStorage.getItem("image"))
+            }, false);
+            
+            if (imgPath) {
+                reader.readAsDataURL(imgPath);
+            }
+        } else {
+            alert("Select Image in order to change it")
+        }
+    } else {
+        $("#display-image").attr("src", $("#url-field").val())
+    }
+})  
